b9d815fb5e7f5702b51a2b5c426877403171501a
Eugene Skinner <eskinfrn@gmail.com>
2015-07-07 12:00:46 -0600

    project complete

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var button = document.getElementById('button')
 var tracks = document.getElementsByClassName('track')
+var i = 0;
 
 button.addEventListener('click',function(e){
-  if (i < 2){
-    var i = 0
-    tracks[i].play()
-    i++
-    if (i === 2) {
-      i = 0
-    }
+  if(i <= 2){
+    tracks[i].play();
+  }
+  i++
+  if(i === 3){
+    i = 0;
   }
 })
